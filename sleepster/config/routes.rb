1f8b0f3385d7352a0cbc52d71fa6e40b7b5cffb7
Michael Lyons <michaellyons@Michaels-MBP.bishopdynamics.com>
2021-03-03 20:33:16 -0800

    I am procrastinating on the CSS stuff

--- sleepster/config/routes.rb
+++ sleepster/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resource :session, only: [:new, :create, :destroy]
